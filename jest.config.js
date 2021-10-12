fe9a03bd057af71c148304837c6e0ed4a2ac175b
mamur <mamur.abdullaev@calmid.com>
2021-10-12 12:55:39 +0900

    fix: configuration for jest

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  rootDir: ".",
   moduleNameMapper: {
     "^@src(.*)$": "<rootDir>/src/$1",
     "^@tests(.*)$": "<rootDir>/tests/$1"
