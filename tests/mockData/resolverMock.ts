c5e9605360a11d715cb81faf7895c2de08ad2e2c
mamur <mamur.abdullaev@calmid.com>
2021-10-12 12:56:00 +0900

    feat: att mock data

---
+++ tests/mockData/resolverMock.ts
@@ -0,0 +1,7 @@
+export const attendanceMockData = {
+  roomId: "1",
+  userId: "123",
+  sessionId: "12345",
+  joinTimestamp: "2021-10-08T20:48:40.446Z",
+  leaveTimestamp: "2021-10-08T20:48:40.446Z",
+}
