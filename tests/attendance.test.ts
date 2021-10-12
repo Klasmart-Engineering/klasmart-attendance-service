1f42238dbb785b0cdfebb3afb45880ced8827abd
mamur <mamur.abdullaev@calmid.com>
2021-10-12 20:19:11 +0900

    fix: remove dotenv from unittest

--- tests/attendance.test.ts
+++ tests/attendance.test.ts
@@ -1,15 +1,13 @@
-import "module-alias/register";
 import { Connection } from "typeorm";
-import * as dotenv from "dotenv"
 import { connectPostgres } from "./utils/postgresDB";
 import { graphqlRequest } from "./utils/graphqlRequest";
-import { SAVE_ATTENDANCE_MUTATION, GET_CLASS_ATTENDANCE_QUERY } from "./utils/graphql";
+import { SAVE_ATTENDANCE_MUTATION, GET_CLASS_ATTENDANCE_QUERY, GET_USER_ATTENDANCE_QUERY } from "./utils/graphql";
 import { attendanceMockData } from "./mockData/resolverMock";
-dotenv.config();
+
 let connection: Connection;
 // jest.useFakeTimers();
 beforeAll(async () => {
-    connection = await connectPostgres();
+    connection = await connectPostgres(true);
 });
 
 afterAll(async () => {
@@ -17,23 +15,41 @@ afterAll(async () => {
 });
 
 describe("queries", () => {
-    it("save Attendance", async () => {
-      const savedAttendance = await graphqlRequest({
+    it("test Attendance", async () => {
+
+      // save attendance
+      await graphqlRequest({
         source: SAVE_ATTENDANCE_MUTATION,
         variableValues: attendanceMockData
       });
 
-      console.log(savedAttendance.data?.savedAttendance);
-
+      // get attendance by roomId
       const getClassAttendance = await graphqlRequest({
         source: GET_CLASS_ATTENDANCE_QUERY,
         variableValues: { roomId: attendanceMockData.roomId }
       });
+      const singleClassAttendace = getClassAttendance.data?.getClassAttendance[0];
+      expect(singleClassAttendace).toMatchObject({
+            roomId: attendanceMockData.roomId,
+            userId: attendanceMockData.userId,
+            sessionId: attendanceMockData.sessionId,
+            joinTimestamp: attendanceMockData.joinTimestamp,
+            leaveTimestamp: attendanceMockData.leaveTimestamp
+      });
 
-
-      console.log(getClassAttendance.data);
+      // get attendance bu userId
+      const getUserAttendane = await graphqlRequest({
+        source: GET_USER_ATTENDANCE_QUERY,
+        variableValues: { userId: attendanceMockData.userId }
+      });
+      
+      const singleUserAttendace = getUserAttendane.data?.getUserAttendance[0];
+      expect(singleUserAttendace).toMatchObject({
+            roomId: attendanceMockData.roomId,
+            userId: attendanceMockData.userId,
+            sessionId: attendanceMockData.sessionId,
+            joinTimestamp: attendanceMockData.joinTimestamp,
+            leaveTimestamp: attendanceMockData.leaveTimestamp
+      });
     });
-
-
-
 });
