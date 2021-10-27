e7044ac1e0ebbdb4ef8b7eb3a976db96cba73b20
mamur <mamur.abdullaev@calmid.com>
2021-10-27 10:50:29 +0900

    chore: print port number

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ const main = async () => {
   server.applyMiddleware({ app });
   
   app.listen(process.env.PORT || 3000, () => {
-    console.log("Server started on http://localhost:3000/graphql")
+    console.log(`Server started on http://localhost:${process.env.PORT || 3000}/graphql`)
   });
 }
 
