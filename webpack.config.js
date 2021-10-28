cb33cd4f02e2cd86ad6694fdc22db1510cd050fd
mamur <mamur.abdullaev@calmid.com>
2021-10-28 10:33:09 +0900

    fix: set mode development, it is causing entity names shortening

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,9 @@ const { IgnorePlugin } = require('webpack');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-    mode: 'production',
+    // TODO: if mode is set to 'production' entity names are changing 
+    // like attendance -> s,  feedback -> d. probably webpack is shortening names
+    mode: 'development',
     target: 'node',
     entry: ['./src/index.ts'],
     devtool: 'source-map',
