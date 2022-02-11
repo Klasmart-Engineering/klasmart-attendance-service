6fc585dea9fa27624f7aad32d20023a8ec0e5b76
mamur <mamur.abdullaev@calmid.com>
2022-02-11 12:01:48 +0900

    chore: add logs

--- src/migrations/1641536505100-CreateDatabase.ts
+++ src/migrations/1641536505100-CreateDatabase.ts
@@ -4,9 +4,10 @@ export class CreateDatabase1641536505100 implements MigrationInterface {
     name = 'CreateDatabase1641536505100'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        
+        console.log(`\x1b[35m`, `running migrations\n`);
         const feedback = await queryRunner.query(`SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'feedback'`);
         if(feedback) {
+            console.log(`\x1b[36m%s\x1b[0m`, `could not find feedback table, creating one ...`);
             await queryRunner.query(`CREATE TYPE "public"."feedback_type_enum" AS ENUM('leave_class', 'end_class')`);
             await queryRunner.query(`CREATE TABLE "feedback" ("session_id" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "room_id" character varying, "user_id" character varying NOT NULL, "type" "public"."feedback_type_enum" NOT NULL, "stars" integer NOT NULL, "comment" character varying, CONSTRAINT "PK_e5bfaf86ab81e5ce5c1b54559d7" PRIMARY KEY ("session_id"))`);
             await queryRunner.query(`CREATE INDEX "IDX_2c2bc370a4c6a7f2f579cd8499" ON "feedback" ("room_id") `);
@@ -14,13 +15,20 @@ export class CreateDatabase1641536505100 implements MigrationInterface {
             await queryRunner.query(`CREATE TYPE "public"."quick_feedback_type_enum" AS ENUM('video', 'audio', 'presentation', 'other')`);
             await queryRunner.query(`CREATE TABLE "quick_feedback" ("id" SERIAL NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "type" "public"."quick_feedback_type_enum" NOT NULL, "stars" integer NOT NULL, "feedbackSessionId" character varying, CONSTRAINT "PK_f171e0608de254b207d1214ab1f" PRIMARY KEY ("id"))`);
             await queryRunner.query(`ALTER TABLE "quick_feedback" ADD CONSTRAINT "FK_a74d52d0b376097a7cd31c8104d" FOREIGN KEY ("feedbackSessionId") REFERENCES "feedback"("session_id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+            console.log(`\x1b[36m%s\x1b[0m`, `table created!`);
+        }else{
+            console.log(`\x1b[36m%s\x1b[0m`, `feedback table exist skipping`);
         }
 
         const attendance = await queryRunner.query(`SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'attendance'`);
         if(attendance){
+            console.log(`\x1b[36m%s\x1b[0m`,`\ncould not find attendance table, creating one ...`);
             await queryRunner.query(`CREATE TABLE "attendance" ("session_id" character varying NOT NULL, "join_timestamp" TIMESTAMP NOT NULL, "leave_timestamp" TIMESTAMP NOT NULL, "room_id" character varying, "user_id" character varying NOT NULL, CONSTRAINT "PK_6faeaae2bb6960b5ca7728ac6c8" PRIMARY KEY ("session_id", "join_timestamp", "leave_timestamp"))`);
             await queryRunner.query(`CREATE INDEX "IDX_7e820f3d6344144d583e6101d4" ON "attendance" ("room_id") `);
             await queryRunner.query(`CREATE INDEX "IDX_0bedbcc8d5f9b9ec4979f51959" ON "attendance" ("user_id") `);
+            console.log(`\x1b[36m%s\x1b[0m`,`table created!`);
+        }else{
+            console.log(`\x1b[36m%s\x1b[0m`, `attendance table exist skipping`);
         }
     }
 
