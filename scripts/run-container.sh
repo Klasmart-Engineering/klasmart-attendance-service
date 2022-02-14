1a3d238fed4a22e46adbcc072e0e95257237f366
mamur <mamur.abdullaev@calmid.com>
2022-02-14 14:52:39 +0900

    chore: use correct name for docker image

--- scripts/run-container.sh
+++ scripts/run-container.sh
@@ -4,10 +4,10 @@ pip3 install -U awscli
 
 aws configure set default.region ap-northeast-2
 
-docker build -t kidsloop-attribute .
+docker build -t kidsloop-attendance .
 
 docker run --add-host host.docker.internal:$BITBUCKET_DOCKER_HOST_INTERNAL \
     -d \
     --name=check-startup \
-    kidsloop-attribute && sleep 8 && docker logs check-startup && docker top check-startup
+    kidsloop-attendance && sleep 8 && docker logs check-startup && docker top check-startup
     
