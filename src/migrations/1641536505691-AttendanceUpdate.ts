dbb34eb4064e8112cc7a937dd4c2aa0fffefd8fb
mamur <mamur.abdullaev@calmid.com>
2022-02-14 22:24:00 +0900

    chore: check if column exist before migration

--- src/migrations/1641536505691-AttendanceUpdate.ts
+++ src/migrations/1641536505691-AttendanceUpdate.ts
@@ -3,7 +3,14 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 export class AttendanceUpdate1641536505691 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE attendance ADD COLUMN "is_teacher" BOOLEAN DEFAULT FALSE`);
+        const query = await queryRunner.query(`SELECT 1 
+                FROM 
+                    information_schema.columns 
+                WHERE 
+                    table_name='attendance' and column_name='is_teacher';`);
+        if(query.length === 0){
+            await queryRunner.query(`ALTER TABLE attendance ADD COLUMN "is_teacher" BOOLEAN DEFAULT FALSE`);
+        }
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
